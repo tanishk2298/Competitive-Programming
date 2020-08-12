c112a72cacb6480356feb196317a7328fe7c6e37
Tanishk Sharma <65224803+tanishk2298@users.noreply.github.com>
2020-08-12 23:20:15 +0530

    Add files via upload

---
+++ GFG/Recursion/Kth symbol in a grammar using recursion.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+public class abcd {	
+	public static void main (String[] args)throws IOException {
+		Scanner sc = new Scanner(System.in);
+		int cases = sc.nextInt();
+		while(cases--> 0){
+			int n = sc.nextInt();
+			int k = sc.nextInt();
+			System.out.println(solve(n,k));
+		}
+	}
+	public static int solve(int n, int k){
+		if(n ==1 && k == 1)
+			return 0;
+		int mid = (int)(Math.pow(2, n-1))/2;
+		int ans = 0;
+		if(k<=mid)
+			ans = solve(n-1,k);
+		else
+			ans = solve(n-1,k-mid) == 0 ? 1 : 0;
+		return ans;
+	}
+}
