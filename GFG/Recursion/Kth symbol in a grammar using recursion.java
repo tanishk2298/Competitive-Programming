e6a872ebbc44116b0bc34abee7e467d4ebe88537
Tanishk Sharma <65224803+tanishk2298@users.noreply.github.com>
2020-08-15 17:26:02 +0530

    Update Kth symbol in a grammar using recursion.java

--- GFG/Recursion/Kth symbol in a grammar using recursion.java
+++ GFG/Recursion/Kth symbol in a grammar using recursion.java
@@ -1,7 +1,7 @@
 import java.util.*;
 import java.io.*;
 
-public class abcd {	
+public class GFG {	
 	public static void main (String[] args)throws IOException {
 		Scanner sc = new Scanner(System.in);
 		int cases = sc.nextInt();
