8797d3fdb5b05cb8cbaf20adf9c83fa1c49a3595
Tanishk Sharma <65224803+tanishk2298@users.noreply.github.com>
2020-08-16 17:21:24 +0530

    Update Subset with spaces of a string.java

--- GFG/Recursion/Subset with spaces of a string.java
+++ GFG/Recursion/Subset with spaces of a string.java
@@ -5,17 +5,19 @@ public class GFG {
 
 	public static void main (String[] args)throws IOException {
 		Scanner sc = new Scanner(System.in);
-		int cases = sc.nextInt();
+        int cases = sc.nextInt();
+        sc.nextLine();
 		while(cases-->0){
 			String ip = sc.nextLine();
 			String op = "";
-			subsetWithSpace(ip, op);
+			subsetWithSpace(ip.substring(1), op+ip.charAt(0));
+			System.out.println("");
 		}
 	}
 
 	public static void subsetWithSpace(String ip, String op){
 		if(ip.length() == 0){
-			System.out.println(op);
+			System.out.print("("+op+")");
 			return;
 		}
 		subsetWithSpace(ip.substring(1), op+" "+ip.charAt(0));
