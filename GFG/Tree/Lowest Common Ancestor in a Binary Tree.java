3e3004021f30e751cbfd3ff5c6f090b61b50ed55
Tanishk Sharma <65224803+tanishk2298@users.noreply.github.com>
2020-08-29 16:15:58 +0530

    Update Lowest Common Ancestor in a Binary Tree.java

--- GFG/Tree/Lowest Common Ancestor in a Binary Tree.java
+++ GFG/Tree/Lowest Common Ancestor in a Binary Tree.java
@@ -3,16 +3,16 @@ class Tree
 	Node lca(Node root, int n1,int n2)
 	{
 		if(root == null)
-		    return null;
+		    	return null;
 		if(root.data == n1 || root.data == n2)
-		    return root;
+		    	return root;
 		Node a = lca(root.left, n1, n2);
-        Node b = lca(root.right, n1, n2);
-        if(a != null && b != null)
-            return root;
-        if(a != null)
-            return a;
-        else 
-            return b;
+        	Node b = lca(root.right, n1, n2);
+        	if(a != null && b != null)
+            		return root;
+        	if(a != null)
+            		return a;
+        	else 
+            		return b;
 	}
 }
