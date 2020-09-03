5d94f47d10331e9a8f7d292d8495fda640f28f53
Tanishk Sharma <65224803+tanishk2298@users.noreply.github.com>
2020-09-03 23:39:19 +0530

    Add files via upload

---
+++ GFG/Binary Search Tree/Brothers From Different Roots - Using Binary Search.java
@@ -0,0 +1,114 @@
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG
+{
+    
+    public static int countPairs(Node root1, Node root2, int x)
+    {
+        ArrayList<Integer> arr1 = new ArrayList<>();
+        ArrayList<Integer> arr2 = new ArrayList<>();
+        fillArr(root1, arr1);
+        fillArr(root2, arr2);
+        int count = 0;
+        for(int i=0;i<arr1.size();i++){
+            int curr = arr1.get(i);
+            if(search(arr2, x-curr)){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void fillArr(Node root, ArrayList<Integer> arr){
+        if(root == null)
+            return;
+        fillArr(root.left, arr);
+        arr.add(root.data);
+        fillArr(root.right, arr);
+    }
+
+    public static boolean search(ArrayList<Integer> arr, int num){
+        int low = 0, high = arr.size()-1;
+        while(low <= high){
+            int mid = low + (high-low)/2;
+            int curr = arr.get(mid);
+            if(curr == num)
+                return true;
+            else if(curr < num)
+                low = mid+1;
+            else
+                high = mid-1;
+        }
+        return false;
+    }
+}
+
+//Driver Function
+
+
+class GFG {
+    
+    static Node buildTree(String str){
+        if(str.length()==0 || str.charAt(0)=='N')
+            return null;
+        String ip[] = str.split(" ");
+        Node root = new Node(Integer.parseInt(ip[0]));
+        Queue<Node> queue = new LinkedList<>(); 
+        queue.add(root);
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            Node currNode = queue.peek();
+            queue.remove();
+            String currVal = ip[i];
+            if(!currVal.equals("N")) {
+                currNode.left = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.left);
+            }
+            i++;
+            if(i >= ip.length)
+                break;
+            currVal = ip[i];
+            if(!currVal.equals("N")) {
+                currNode.right = new Node(Integer.parseInt(currVal));
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        return root;
+    }
+
+    static void printInorder(Node root){
+        if(root == null)
+            return;
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        printInorder(root.right);
+    }
+
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+        while(t--> 0){
+            String s = br.readLine();
+            Node root = buildTree(s);
+            GfG g = new GfG();
+            Node rootA = g.binaryTreeToBST(root);
+            printInorder(rootA);
+            System.out.println();
+        }
+    }
+}
