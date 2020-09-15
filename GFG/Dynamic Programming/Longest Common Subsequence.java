f7557080ccf8e28c67d0ce60bfe7a1ba344654ba
Tanishk Sharma <65224803+tanishk2298@users.noreply.github.com>
2020-09-15 21:55:34 +0530

    Add files via upload

---
+++ GFG/Dynamic Programming/Longest Common Subsequence.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG{
+	public static void main(String[] args)throws IOException{
+	    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	    int tc = Integer.parseInt(br.readLine().trim());
+	    while(tc-->0){
+	        String ip[] = br.readLine().trim().split(" ");
+	        int m = Integer.parseInt(ip[0]);
+	        int n = Integer.parseInt(ip[1]);
+	        String x = br.readLine();
+	        String y = br.readLine();
+	        int[][] dp = new int[m+1][n+1];
+	        for(int i=0;i<=m;i++){
+	            for(int j=0;j<=n;j++){
+	                if(i==0||j==0)
+	                    dp[i][j] = 0;
+	                else{
+	                    if(x.charAt(i-1) == y.charAt(j-1))
+	                        dp[i][j] = 1 + dp[i-1][j-1];
+	                    else
+	                        dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+	                }
+	            }
+	        }
+	        System.out.println(dp[m][n]);
+	    }        
+	}
+}
