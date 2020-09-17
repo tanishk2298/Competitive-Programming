afe1fec13bb7817054f1612d7ddf17f80aaf72ba
Tanishk Sharma <65224803+tanishk2298@users.noreply.github.com>
2020-09-17 21:21:39 +0530

    Add files via upload

---
+++ GFG/Dynamic Programming/Minimum number of deletion to make a string palindromic.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG{
+    public static void main (String[] args){
+	Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while(t-->0){
+            String x = sc.next();
+            StringBuilder y = new StringBuilder(x);
+            y.reverse();
+            int m = x.length();
+            int n = y.length();
+            int[][] dp = new int[m+1][n+1];
+            for(int i=0;i<=m;i++){
+                for(int j=0;j<=n;j++){
+                    if(i==0||j==0)
+                        dp[i][j] = 0;
+                    else if(x.charAt(i-1) == y.charAt(j-1))
+                    	dp[i][j] = 1 + dp[i-1][j-1];
+                    else
+                    	dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+            	}
+            }
+            System.out.println(m-dp[m][n]);
+        }
+    }
+}
