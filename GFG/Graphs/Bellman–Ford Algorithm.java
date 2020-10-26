a0b4acea5664ffcbbdc112498f65c2c6d70b01bf
Tanishk Sharma <65224803+tanishk2298@users.noreply.github.com>
2020-10-26 21:21:30 +0530

    Add files via upload

---
+++ GFG/Graphs/Bellman–Ford Algorithm.java
@@ -0,0 +1,50 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+class GFG
+{
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        int tc = sc.nextInt();
+        while(tc-->0){
+            int v = sc.nextInt();
+            int e = sc.nextInt();
+            String[] graph = new String[e];
+            for(int i=0;i<e;i++){
+                int src = sc.nextInt();
+                int dest = sc.nextInt();
+                int wt = sc.nextInt();
+                graph[i] = src+" "+dest+" "+wt;
+            }
+            if(bellmanFord(graph,v,e))
+                System.out.println("Graph contains negative weight cycle");
+            else
+                System.out.println("Graph doesn't contains negative weight cycle");            
+        }
+    }
+    public static boolean bellmanFord(String[] graph, int v, int e){
+        int[] cost_parent = new int[v];
+	int[] val = new int[v];
+	Arrays.fill(val, Integer.MAX_VALUE);
+	val[0] = 0;
+	for(int i=0;i<(v-1);i++){
+	    for(int j=0;j<e;j++){
+	        String[] sdw = graph[j].split(" ");
+	        int src = Integer.parseInt(sdw[0]);
+	        int dest = Integer.parseInt(sdw[1]);;
+	        int wt = Integer.parseInt(sdw[2]);
+	        if(val[src] != Integer.MAX_VALUE && (val[src] + wt) < val[dest])
+	            val[dest] = val[src] + wt;
+	    }
+	}
+	for(int i=0;i<e;i++){
+	    String[] sdw = graph[i].split(" ");
+	    int src = Integer.parseInt(sdw[0]);
+	    int dest = Integer.parseInt(sdw[1]);
+	    int wt = Integer.parseInt(sdw[2]);  
+	    if(val[src] != Integer.MAX_VALUE && (val[src] + wt) < val[dest])
+	        return true;
+	}
+	return false;
+    }
+}
