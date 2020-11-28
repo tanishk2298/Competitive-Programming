6a303fedd5f32fab8fabae015e5ba8d04edcc5ee
Tanishk Sharma <65224803+tanishk2298@users.noreply.github.com>
2020-11-28 23:27:31 +0530

    Update 3 - Manna's First Name.java

--- CP List/Linear Search/3 - Manna's First Name.java
+++ CP List/Linear Search/3 - Manna's First Name.java
@@ -9,15 +9,14 @@ class TestClass
    {
        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
        int l,s1,s2,t=Integer.parseInt(br.readLine());
-       while(t-->0)
-       {
-           String s=br.readLine();
-           l=s.length();
-           s=s.replace("SUVOJIT","");
-           s1=(l-s.length())/7;
-           l=s.length();
-           s=s.replace("SUVO","");
-           s2=(l-s.length())/4;
+       while(t-->0){
+           String s = br.readLine();
+           l = s.length();
+           s = s.replace("SUVOJIT","");
+           s1 = (l-s.length()) / 7;
+           l = s.length();
+           s = s.replace("SUVO","");
+           s2 = (l-s.length()) / 4;
            System.out.println("SUVO = "+s2+", SUVOJIT = "+s1);
        }
    }
