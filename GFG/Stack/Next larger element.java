de01924ed3c387e2dfa691cd164b45639c0d4978
Tanishk Sharma <65224803+tanishk2298@users.noreply.github.com>
2020-12-29 22:33:32 +0530

    Update Next larger element.java

--- GFG/Stack/Next larger element.java
+++ GFG/Stack/Next larger element.java
@@ -4,21 +4,17 @@ import java.lang.*;
 
 class solve{
     public static long[] nextLargerElement(long[] arr, int n) { 
+        Stack<Long> stk = new Stack<>();
         long[] ans = new long[n];
-        for(int i=0;i<n-1;i++){
-            long curr = arr[i];
-            boolean flag = false;
-            for(int j=(i+1);j<n;j++){
-                if(arr[j] > curr){
-                    ans[i] = arr[j];
-                    flag = true;
-                    break;
-                }
-            }
-            if(!flag)
+        for(int i=(n-1);i>=0;i--){
+            while(!stk.isEmpty() && arr[i] >= stk.peek())
+                stk.pop();
+            if(stk.isEmpty())
                 ans[i] = -1;
+            else
+                ans[i] = stk.peek();
+            stk.push(arr[i]);
         }
-        ans[n-1] = -1;
         return ans;
     } 
 }
