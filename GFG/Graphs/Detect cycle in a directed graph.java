86fec9413f0e2dd19c5d0d600bd96b41a619f485
Tanishk Sharma <65224803+tanishk2298@users.noreply.github.com>
2021-01-24 16:50:42 +0530

    Update Detect cycle in a directed graph.java

--- GFG/Graphs/Detect cycle in a directed graph.java
+++ GFG/Graphs/Detect cycle in a directed graph.java
@@ -4,36 +4,28 @@ import java.io.*;
 
 class DetectCycle
 {
-    static boolean isCyclic(ArrayList<ArrayList<Integer>> adj, int v)
-    {
-        boolean[] isVisited = new boolean[v];
-        Arrays.fill(isVisited, false);
-        boolean flag = false;
+    public boolean isCyclic(int v, ArrayList<ArrayList<Integer>> adj) {
+        boolean[] vis = new boolean[v];
+        boolean[] rec = new boolean[v];
+        boolean ans = false;
         for(int i=0;i<v;i++){
-            isVisited[i] = true;
-            ArrayList<Integer> currList = adj.get(i);
-            for(int j=0;j<currList.size();j++){
-                flag = isCyclicUtil(adj, isVisited, currList.get(j));
-                if(flag == true)
+            if(vis[i] == false)
+                ans = dfs(adj,v,i,vis,rec);    
+        }
+        return ans;
+    }
+    public static boolean dfs(ArrayList<ArrayList<Integer>> adj, int v, int curr, boolean[] vis, boolean[] rec){
+        if(vis[curr] == false){
+            vis[curr] = true;
+            rec[curr] = true;
+            for(Integer i : adj.get(curr)){
+                if(!vis[i] && dfs(adj,v,i,vis,rec))
+                    return true;
+                else if(rec[i] == true)
                     return true;
             }
-            isVisited[i] = false;
         }
-        return false;
-    }
-    
-    static boolean isCyclicUtil(ArrayList<ArrayList<Integer>> adj, boolean[] isVisited, int curr){
-        if(isVisited[curr] == true)
-            return true;
-        isVisited[curr] = true;
-        boolean flag = false;
-        ArrayList<Integer> currList = adj.get(curr);
-        for(int i=0;i<currList.size();i++){
-            flag = isCyclicUtil(adj, isVisited, currList.get(i));
-            if(flag == true)
-                return true;
-        }
-        isVisited[curr] = false;
+        rec[curr] = false;
         return false;
     }
 }
